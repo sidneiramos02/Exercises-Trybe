0fddeff5ccff3a058ed913c8ec4924912fee0f21
Sidnei Ramos <sidneiramos02@gmail.com>
2020-05-17 19:03:47 -0300

    Start exercise objects

---
+++ exercises/4_4/objetos.js
@@ -0,0 +1,17 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
+  };
+
+  console.log("Bem-vinda "+[info.personagem])
+  console.log(info.recorrente)
+
+  for(let i in info) {
+  console.log(i)
+  }
+
+  for(let i in info) {
+  console.log(i.info)
+  }
